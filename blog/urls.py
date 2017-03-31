2cae32c76fb0fd54aa83a1b9929f2a0d43af3021
ashton <humanity.noise@gmail.com>
2017-03-31 12:43:16 +0300

    Testing views

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
+from mashup.blog.views import EntryList, EntryDetail
 
 urlpatterns = [
-
-
+    url(r'^$', EntryList.as_view(), name='entry_list'),
+    url(r'^(?P<page>[0-9]+)$', EntryList.as_view(), name='entry_list')
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,24 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
+from mashup.blog.models import Entry
 
 # Create your views here.
-def entry_list(request):
-    pass
+class EntryList(ListView):
+    model = Entry
+    template_name = 'blog/entry_list.html'
+    context_object_name = 'entries'
+    page_kwarg = 'page'
+    paginate_by = 10
+
+    def get(self, request, *args, **kwargs):
+        pass
+
+    def get_queryset(self):
+        pass
+
+
+class EntryDetail(DetailView):
+    model = Entry
+    template_name = 'blog/entry_detail.html'
+    context_object_name = 'entry'
